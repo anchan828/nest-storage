e649ce636df217af8b1b2db6c4d29029bef26eda
anchan828 <anchan828@gmail.com>
2020-01-06 19:32:33 +0900

    feat: export AbstractStorage

--- packages/storage/src/index.ts
+++ packages/storage/src/index.ts
@@ -1,3 +1,4 @@
+export { AbstractStorage } from "@anchan828/nest-storage-common";
 export { LocalStorage } from "./local.storage";
 export { StorageModule } from "./storage.module";
 export { StorageService } from "./storage.service";
