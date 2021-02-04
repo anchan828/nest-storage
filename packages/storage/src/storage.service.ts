70e8f000b96000164f7c5874c85103a2d8e6f654
Keigo Ando <anchan828@gmail.com>
2021-02-04 17:23:50 +0900

    chore: remove p-map

--- packages/storage/src/storage.service.ts
+++ packages/storage/src/storage.service.ts
@@ -8,7 +8,6 @@ import { STORAGE_DEFAULT_SIGNED_URL_EXPIRES, STORAGE_PROVIDER } from "@anchan828
 import { Inject, Injectable } from "@nestjs/common";
 import * as compressing from "compressing";
 import { createWriteStream } from "fs";
-import * as pMap from "p-map";
 import { tmpNameSync } from "tmp";
 import type { CompressFileEntry, CompressOptions, CompressType } from "./interfaces";
 @Injectable()
@@ -33,12 +32,12 @@ export class StorageService {
     const stream = this.getCompressStream(options.compressType);
     const dest = tmpNameSync({ postfix: this.getCompressFileExtension(options.compressType) });
 
-    await pMap(entries, async (entry) => {
+    for (const entry of entries) {
       const filename = typeof entry === "string" ? entry : entry.filename;
       const relativePath = typeof entry === "string" ? entry : entry.relativePath;
       const dataPath = await this.storage.download(filename, options);
       stream.addEntry(dataPath, { relativePath: relativePath.replace(/^\//g, "") });
-    });
+    }
 
     stream.pipe(createWriteStream(dest));
 
